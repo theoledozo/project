d5cd0b00fa5a43812fcbd594c5784fef928fb1b9
theoledozo <71723213+theoledozo@users.noreply.github.com>
2025-03-09 20:32:30 +0800

    L'url et la clé sont maintenant dans le fichier .env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+if (!import.meta.env.VITE_SUPABASE_URL) {
+  throw new Error('VITE_SUPABASE_URL is not defined');
+}
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+if (!import.meta.env.VITE_SUPABASE_ANON_KEY) {
+  throw new Error('VITE_SUPABASE_ANON_KEY is not defined');
+}
+
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
