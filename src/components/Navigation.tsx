41b2c9f8f126779b110495144d463ad98a8e663d
theoledozo <71723213+theoledozo@users.noreply.github.com>
2025-03-12 18:07:58 +0800

    span supprimer

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,27 +7,21 @@ const Navigation = () => {
       <div className="flex justify-around p-2">
         <Link to="/" className="flex flex-col items-center">
           <Home size={24} />
-          <span>Accueil</span>
         </Link>
         <Link to="/shopping-list" className="flex flex-col items-center">
           <ListChecks size={24} />
-          <span>Courses</span>
         </Link>
         <Link to="/inventory" className="flex flex-col items-center">
           <Package size={24} />
-          <span>Inventaire</span>
         </Link>
         <Link to="/recipes" className="flex flex-col items-center">
           <ChefHat size={24} />
-          <span>Recettes</span>
         </Link>
         <Link to="/todo" className="flex flex-col items-center">
           <CheckSquare size={24} />
-          <span>Todo</span>
         </Link>
         <Link to="/88-days" className="flex flex-col items-center">
           <Calendar size={24} />
-          <span>88 Jours</span>
         </Link>
       </div>
     </nav>
